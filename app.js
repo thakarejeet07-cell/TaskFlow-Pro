584e43f052fc3b588722a7ae0784980f6e775294
Jeet Thakare <thakarejeet07@gmail.com>
2026-07-27 15:15:49 +0530

    Day 4: localStorage save/load persistence

--- app.js
+++ app.js
@@ -37,21 +37,32 @@ function addListToBoard(board,list){
     board.lists.push(list);
 }
 
-const board = createBoard("My Project");
+// const board = createBoard("My Project");
 
-const todoList = createList("To Do");
-const doingList = createList("Doing");
-const doneList  = createList("Done");
+// const todoList = createList("To Do");
+// const doingList = createList("Doing");
+// const doneList  = createList("Done");
 
-addListToBoard(board, todoList);
-addListToBoard(board, doingList);
-addListToBoard(board, doneList);
+// addListToBoard(board, todoList);
+// addListToBoard(board, doingList);
+// addListToBoard(board, doneList);
 
-const card1 = createCard("Fix login bug", "Login button not working on Safari");
-const card2 = createCard("Write blog post", "About JS event loop");
+// const card1 = createCard("Fix login bug", "Login button not working on Safari");
+// const card2 = createCard("Write blog post", "About JS event loop");
 
-addCardToList(todoList,card1);
-addCardToList(todoList,card2);
+
+// addCardToList(todoList,card1);
+// addCardToList(todoList,card2);
+
+let board = loadBoard();
+
+if(!board){
+    board = createBoard("My Project");
+    addListToBoard(board, createList("To Do"));
+    addListToBoard(board, createList("Doing"));
+    addListToBoard(board, createList("Done"));
+}
+
 
 // console.log(board);
 
@@ -74,9 +85,32 @@ function renderBoard(board){
             listDiv.appendChild(cardEl);
         });
 
-        boardEl.appendChild(listDiv);
+    const addButton = document.createElement("button")
+    addButton.textContent = "+ Add Card"
+
+    addButton.addEventListener("click",() => {
+        const title =  prompt("Enter card title");
+        if(!title) return;
+
+        const newCard = createCard(title,"");
+        addCardToList(list,newCard);
+        saveBoard(board);
+        renderBoard(board);
+    });
+    listDiv.appendChild(addButton);
+    boardEl.appendChild(listDiv)
     });
 }
+
+function saveBoard(board){
+    localStorage.setItem("taskFlow-board",JSON.stringify(board));
+}
+function loadBoard(){
+    const data = localStorage.getItem('taskFlow-board');
+    if(!data) return null;
+    return JSON.parse(data);
+}
+
 
 renderBoard(board);
 
